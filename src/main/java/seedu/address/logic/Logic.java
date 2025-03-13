2490061599fc951ea291b5ef499d070d0f3ee5c6
Beryl <a.j.beryl@gmail.com>
2025-03-14 01:16:43 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -9,7 +9,7 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
-import seedu.address.model.wedding.Wedding;;
+import seedu.address.model.wedding.Wedding;
 
 /**
  * API of the Logic component
