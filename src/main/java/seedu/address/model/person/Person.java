23a58ebaaed8d8408d85304c2ed1d4e1532a309a
Beryl <a.j.beryl@gmail.com>
2025-03-16 21:13:37 +0800

    update classes to add 'tag' functionality

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -31,14 +31,6 @@ public class Person {
      */
     public Person(Name name, Phone phone, Email email, Role role, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, role, address, tags);
-
-        boolean hasRequiredTag = tags.stream()
-                .anyMatch(tag -> tag.tagName.equals("Client") || tag.tagName.equals("Vendor"));
-
-        if (!hasRequiredTag) {
-            throw new IllegalArgumentException("A person must have at least one tag: 'Client' or 'Vendor'.");
-        }
-
         this.name = name;
         this.phone = phone;
         this.email = email;
@@ -86,6 +78,19 @@ public class Person {
 
         return otherPerson != null
                 && otherPerson.getName().equals(getName());
+    }
+
+    /**
+     * Returns a new Person with the WeddingId tag added.
+     *
+     * @param tag The tag to be added.
+     * @return A new Person instance with the tag added.
+     */
+    public Person withAddedTag(Tag tag) {
+        requireAllNonNull(tag);
+        Set<Tag> newTags = new HashSet<>(this.tags);
+        newTags.add(tag);
+        return new Person(name, phone, email, role, address, newTags);
     }
 
     /**
