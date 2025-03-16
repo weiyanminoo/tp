fcd207106986ea4c8550db96cbb8885bf6cc7219
Beryl <a.j.beryl@gmail.com>
2025-03-16 21:16:58 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -21,24 +21,24 @@ import seedu.address.model.wedding.WeddingId;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
-                new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                        new Role("Groom"), new Address("Blk 30 Geylang Street 29, #06-40"),
-                        getTagSet()),
-                new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                        new Role("Bride"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                        getTagSet()),
-                new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                        new Role("Bride"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                        getTagSet()),
-                new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                        new Role("Groom"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                        getTagSet()),
-                new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                        new Role("Caterer"), new Address("Blk 47 Tampines Street 20, #17-35"),
-                        getTagSet()),
-                new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                        new Role("Photographer"), new Address("Blk 45 Aljunied Street 85, #11-31"),
-                        getTagSet())
+            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                    new Role("Groom"), new Address("Blk 30 Geylang Street 29, #06-40"),
+                    getTagSet()),
+            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                    new Role("Bride"), new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                    getTagSet()),
+            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+                    new Role("Bride"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                    getTagSet()),
+            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                    new Role("Groom"), new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                    getTagSet()),
+            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+                    new Role("Caterer"), new Address("Blk 47 Tampines Street 20, #17-35"),
+                    getTagSet()),
+            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+                    new Role("Photographer"), new Address("Blk 45 Aljunied Street 85, #11-31"),
+                    getTagSet())
         };
     }
 
