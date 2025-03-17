68411136bb003ac92a7d2ad7371e8db21ca7039f
Beryl <a.j.beryl@gmail.com>
2025-03-17 16:26:15 +0800

    Modify and add new tests

--- src/test/java/seedu/address/model/person/PersonTest.java
+++ src/test/java/seedu/address/model/person/PersonTest.java
@@ -8,7 +8,7 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ROLE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_VENDOR;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_BOB;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BOB;
@@ -35,7 +35,7 @@ public class PersonTest {
 
         // same name, all other attributes different -> returns true
         Person editedAlice = new PersonBuilder(ALICE).withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB)
-                .withRole(VALID_ROLE_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_VENDOR).build();
+                .withRole(VALID_ROLE_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_BOB).build();
         assertTrue(ALICE.isSamePerson(editedAlice));
 
         // different name, all other attributes same -> returns false
@@ -91,7 +91,7 @@ public class PersonTest {
         assertFalse(ALICE.equals(editedAlice));
 
         // different tags -> returns false
-        editedAlice = new PersonBuilder(ALICE).withTags(VALID_TAG_VENDOR).build();
+        editedAlice = new PersonBuilder(ALICE).withTags(VALID_TAG_BOB).build();
         assertFalse(ALICE.equals(editedAlice));
     }
 
--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -10,8 +10,7 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ROLE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ROLE_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_CLIENT;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_VENDOR;
+// Since tags are assigned via the tag command, we remove the tag constants.
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,39 +24,103 @@ import seedu.address.model.person.Person;
  */
 public class TypicalPersons {
 
-    public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
-            .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
-            .withPhone("94351253").withRole("florist")
-            .withTags().build();
-    public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
-            .withAddress("311, Clementi Ave 2, #02-25").withRole("caterer")
-            .withEmail("johnd@example.com").withPhone("98765432")
-            .withTags("Client").build();
-    public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withRole("photographer").withAddress("wall street")
-            .withTags("Client").build();
-    public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withRole("groom").withAddress("10th street").withTags("Client").build();
-    public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withRole("bride").withAddress("michegan ave").withTags("Client").build();
-    public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withRole("stylist").withAddress("little tokyo").withTags("Client").build();
-    public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withRole("host").withAddress("4th street").withTags("Vendor").build();
+    // No tags are assigned by default since tagging is now handled via the tag command.
+    public static final Person ALICE = new PersonBuilder()
+            .withName("Alice Pauline")
+            .withAddress("123, Jurong West Ave 6, #08-111")
+            .withEmail("alice@example.com")
+            .withPhone("94351253")
+            .withRole("florist")
+            .withTags()
+            .build();
 
-    // Manually added
-    public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("little india").withTags("Client").build();
-    public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("chicago ave").withTags("Client").build();
+    public static final Person BENSON = new PersonBuilder()
+            .withName("Benson Meier")
+            .withAddress("311, Clementi Ave 2, #02-25")
+            .withRole("caterer")
+            .withEmail("johnd@example.com")
+            .withPhone("98765432")
+            .withTags()
+            .build();
+
+    public static final Person CARL = new PersonBuilder()
+            .withName("Carl Kurz")
+            .withPhone("95352563")
+            .withEmail("heinz@example.com")
+            .withRole("photographer")
+            .withAddress("wall street")
+            .withTags()
+            .build();
+
+    public static final Person DANIEL = new PersonBuilder()
+            .withName("Daniel Meier")
+            .withPhone("87652533")
+            .withEmail("cornelia@example.com")
+            .withRole("groom")
+            .withAddress("10th street")
+            .withTags()
+            .build();
+
+    public static final Person ELLE = new PersonBuilder()
+            .withName("Elle Meyer")
+            .withPhone("9482224")
+            .withEmail("werner@example.com")
+            .withRole("bride")
+            .withAddress("michegan ave")
+            .withTags()
+            .build();
+
+    public static final Person FIONA = new PersonBuilder()
+            .withName("Fiona Kunz")
+            .withPhone("9482427")
+            .withEmail("lydia@example.com")
+            .withRole("stylist")
+            .withAddress("little tokyo")
+            .withTags()
+            .build();
+
+    public static final Person GEORGE = new PersonBuilder()
+            .withName("George Best")
+            .withPhone("9482442")
+            .withEmail("anna@example.com")
+            .withRole("host")
+            .withAddress("4th street")
+            .withTags("W2")
+            .build();
+
+    // Manually added persons.
+    public static final Person HOON = new PersonBuilder()
+            .withName("Hoon Meier")
+            .withPhone("8482424")
+            .withEmail("stefan@example.com")
+            .withAddress("little india")
+            .withTags()
+            .build();
+    public static final Person IDA = new PersonBuilder()
+            .withName("Ida Mueller")
+            .withPhone("8482131")
+            .withEmail("hans@example.com")
+            .withAddress("chicago ave")
+            .withTags()
+            .build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
-    public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
-            .withEmail(VALID_EMAIL_AMY).withRole(VALID_ROLE_AMY).withAddress(VALID_ADDRESS_AMY)
-            .withTags(VALID_TAG_CLIENT).build();
-    public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-            .withEmail(VALID_EMAIL_BOB).withRole(VALID_ROLE_BOB).withAddress(VALID_ADDRESS_BOB)
-            .withTags(VALID_TAG_VENDOR).build();
+    public static final Person AMY = new PersonBuilder()
+            .withName(VALID_NAME_AMY)
+            .withPhone(VALID_PHONE_AMY)
+            .withEmail(VALID_EMAIL_AMY)
+            .withRole(VALID_ROLE_AMY)
+            .withAddress(VALID_ADDRESS_AMY)
+            .withTags()
+            .build();
+    public static final Person BOB = new PersonBuilder()
+            .withName(VALID_NAME_BOB)
+            .withPhone(VALID_PHONE_BOB)
+            .withEmail(VALID_EMAIL_BOB)
+            .withRole(VALID_ROLE_BOB)
+            .withAddress(VALID_ADDRESS_BOB)
+            .withTags()
+            .build();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
