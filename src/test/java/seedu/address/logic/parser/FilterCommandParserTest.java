3086729f6c9c793f80d511b4439b74dbb2f0a4a6
glennliew <glennliew1@gmail.com>
2025-03-17 22:53:25 +0800

    Fix formatting bugs

--- src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterCommandParserTest.java
@@ -3,7 +3,7 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static seedu.address.logic.commands.CommandTestUtil.MESSAGE_INVALID_COMMAND_FORMAT; 
+import static seedu.address.logic.commands.CommandTestUtil.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_BOB;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -25,7 +25,7 @@ public class FilterCommandParserTest {
 
         // Leading and trailing whitespaces
         assertParseSuccess(parser, " \t " + VALID_TAG_AMY + " \t ", expectedFilterCommand);
-        
+
         // Different wedding ID
         FilterCommand anotherExpectedCommand = new FilterCommand(new WeddingId(VALID_TAG_BOB));
         assertParseSuccess(parser, VALID_TAG_BOB, anotherExpectedCommand);
@@ -34,23 +34,23 @@ public class FilterCommandParserTest {
     @Test
     public void parse_invalidArgs_throwsParseException() {
         // Empty string
-        assertParseFailure(parser, "", 
+        assertParseFailure(parser, "",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
-        
+
         // Whitespace only
-        assertParseFailure(parser, "   \t", 
+        assertParseFailure(parser, "   \t",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
-        
+
         // Invalid wedding ID format (if there are specific validation rules for wedding IDs)
         // For example, if wedding IDs must start with 'W' followed by numbers
-        assertParseFailure(parser, "InvalidWeddingId", 
+        assertParseFailure(parser, "InvalidWeddingId",
                 WeddingId.MESSAGE_CONSTRAINTS);
-        
+
         // Multiple arguments (should only accept one wedding ID)
         assertParseFailure(parser, VALID_TAG_AMY + " " + VALID_TAG_BOB,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
     }
-    
+
     @Test
     public void equals() {
         FilterCommand filterFirstCommand = new FilterCommand(new WeddingId(VALID_TAG_AMY));
