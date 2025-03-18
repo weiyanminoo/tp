5e3114d6394b4b4dd0c930aa9099e3161bdfccdb
GrassHeadd <113354001+GrassHeadd@users.noreply.github.com>
2025-03-19 01:05:40 +0800

    add more tests to addressBookParser

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.Messages.MESSAGE_UNKNOWN_COMMAND;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_WEDDING_NAME;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 
@@ -13,15 +14,8 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.EditPersonDescriptorBuilder;
@@ -85,6 +79,30 @@ public class AddressBookParserTest {
     public void parseCommand_list() throws Exception {
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
         assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+    }
+
+    @Test
+    public void parseCommand_filter() throws Exception {
+        String userInput = FilterCommand.COMMAND_WORD + " W1";
+        assertTrue(parser.parseCommand(userInput) instanceof FilterCommand);
+    }
+
+    @Test
+    public void parseCommand_tag() throws Exception {
+        String userInput = TagCommand.COMMAND_WORD + " 1 W123";
+        assertTrue(parser.parseCommand(userInput) instanceof TagCommand);
+    }
+
+    @Test
+    public void parseCommand_addWeddingEvent() throws Exception {
+        String userInput = AddWeddingEventCommand.COMMAND_WORD + " n/Alice & Bob d/01-Dec-2025 l/Paris";
+        assertTrue(parser.parseCommand(userInput) instanceof AddWeddingEventCommand);
+    }
+
+    @Test
+    public void parseCommand_editWeddingEvent() throws Exception {
+        String userInput = EditWeddingEventCommand.COMMAND_WORD + " W3 " + PREFIX_WEDDING_NAME + "New Wedding Name";
+        assertTrue(parser.parseCommand(userInput) instanceof EditWeddingEventCommand);
     }
 
     @Test
