ffc98b5bfcac7a3de2488e65a45bca3535e190e2
gengyudong <gengyudong0155@gmail.com>
2025-03-20 01:38:16 +0800

    Add test to increase coverage

--- src/test/java/seedu/address/logic/parser/DeleteWeddingCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteWeddingCommandParserTest.java
@@ -1,44 +1,44 @@
 package seedu.address.logic.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DeleteWeddingCommand;
+import seedu.address.model.wedding.WeddingId;
 
 public class DeleteWeddingCommandParserTest {
 
-    private final DeleteCommandParser parser = new DeleteCommandParser();
+    private final DeleteWeddingCommandParser parser = new DeleteWeddingCommandParser();
 
     @Test
-    public void parse_validArgs_returnsDeleteCommand() throws Exception {
-        String userInput = "1";
-        Index targetIndex = Index.fromOneBased(1);
-        DeleteCommand expectedCommand = new DeleteCommand(targetIndex);
-        assertParseSuccess(parser, userInput, expectedCommand);
+    public void parse_validArgs_returnsDeleteWeddingCommand() throws Exception {
+        WeddingId weddingId = new WeddingId("W12345");
+        DeleteWeddingCommand command = parser.parse("W12345");
+        assertEquals(new DeleteWeddingCommand(weddingId), command);
     }
 
     @Test
     public void parse_invalidIndex_throwsParseException() {
         String userInput = "a";
         assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                DeleteCommand.MESSAGE_USAGE));
+                DeleteWeddingCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_missingArgs_throwsParseException() {
         String userInput = "";
         assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                DeleteCommand.MESSAGE_USAGE));
+                DeleteWeddingCommand.MESSAGE_USAGE));
     }
 
     @Test
     public void parse_extraArgs_throwsParseException() {
         String userInput = "1 extra";
         assertParseFailure(parser, userInput, String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                DeleteCommand.MESSAGE_USAGE));
+                DeleteWeddingCommand.MESSAGE_USAGE));
     }
 }
