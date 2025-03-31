b4a78b02ad5cba1f5eab56a5624829da63a1ea1d
Beryl <a.j.beryl@gmail.com>
2025-03-31 21:32:14 +0530

    modify boolean naming

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -19,7 +19,7 @@ public class CommandResult {
     /** The application should exit. */
     private final boolean exit;
     private final boolean showWeddingList;
-    private boolean sortWeddingByDate = false;
+    private boolean isSortWeddingByDate = false;
 
     /**
      * Indicates whether this command result requires further user confirmation.
@@ -65,7 +65,7 @@ public class CommandResult {
     }
 
     public boolean isSortWeddingsByDate() {
-        return sortWeddingByDate;
+        return isSortWeddingByDate;
     }
 
     /**
