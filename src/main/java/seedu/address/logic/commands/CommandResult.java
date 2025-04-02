9c763c08c297ac87638738d5c9908c2653a111bb
GrassHeadd <113354001+GrassHeadd@users.noreply.github.com>
2025-04-02 18:26:22 +0800

    checkstyle fix

--- src/main/java/seedu/address/logic/commands/CommandResult.java
+++ src/main/java/seedu/address/logic/commands/CommandResult.java
@@ -41,7 +41,8 @@ public class CommandResult {
     /**
      *  Constructs a {@code CommandResult} with the specified fields.
      */
-    public CommandResult(String feedbackToUser, boolean showHelp, boolean exit, boolean requiresConfirmation, boolean refreshUI) {
+    public CommandResult(String feedbackToUser, boolean showHelp,
+                         boolean exit, boolean requiresConfirmation, boolean refreshUI) {
         this.feedbackToUser = requireNonNull(feedbackToUser);
         this.showHelp = showHelp;
         this.exit = exit;
