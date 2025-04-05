168c586ed99bdc7c03f017eb86c1e1866099f659
Beryl <a.j.beryl@gmail.com>
2025-04-05 23:43:32 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/parser/UntagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/UntagCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_WEDDING_ID;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.UntagCommand;
